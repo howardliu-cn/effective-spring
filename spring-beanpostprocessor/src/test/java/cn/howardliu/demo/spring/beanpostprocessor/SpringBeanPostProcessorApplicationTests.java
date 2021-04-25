27a1b8977dbcaa3cf3dd4796ddec3b2cb1dcbcca
Howard Liu <howardliu1988@163.com>
2021-04-25 22:21:40 +0800

    修改测试用例

--- spring-beanpostprocessor/src/test/java/cn/howardliu/demo/spring/beanpostprocessor/SpringBeanPostProcessorApplicationTests.java
+++ spring-beanpostprocessor/src/test/java/cn/howardliu/demo/spring/beanpostprocessor/SpringBeanPostProcessorApplicationTests.java
@@ -10,12 +10,19 @@ import org.springframework.util.Assert;
 class SpringBeanPostProcessorApplicationTests {
     @IdGeneratorClient
     private IdGenerator defaultIdGenerator;
+    @IdGeneratorClient("group1")
+    private IdGenerator group1IdGenerator;
 
     @Test
     void contextLoads() {
         Assert.notNull(defaultIdGenerator, "注入失败");
-        System.out.println(defaultIdGenerator.groupName());
-        System.out.println(defaultIdGenerator.nextId());
+        System.out.println(defaultIdGenerator.groupName() + " => " + defaultIdGenerator.nextId());
+
+        Assert.notNull(group1IdGenerator, "注入失败");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(defaultIdGenerator.groupName() + " => " + defaultIdGenerator.nextId());
+            System.out.println(group1IdGenerator.groupName() + " => " + group1IdGenerator.nextId());
+        }
     }
 
 }
